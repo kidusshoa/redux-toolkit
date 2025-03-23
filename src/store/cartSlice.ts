b2d9b303cb73111c4971cf4e0d77e81a57d9abd9
kidusshoa <captainkevin2008@gmail.com>
2025-03-23 22:14:50 +0300

    cart slice

---
+++ src/store/cartSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+type CartItem = {
+  id: string;
+  title: string;
+  price: number;
+  image: string;
+};
+
+const initialState: CartItem[] = [];
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    add(state, action) {
+      state.push(action.payload);
+    },
+  },
+});
+
+export default cartSlice.reducer;
